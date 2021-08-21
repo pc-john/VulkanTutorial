c0dee8696ff593ec54db1ada10f52717bd6714ff
pc-john (Jan Pečiva) <peciva@fit.vut.cz>
2021-08-21 08:50:06 +0200

    04-deviceInfo: finalized + published on root.cz .

--- 04-deviceInfo/main.cpp
+++ 04-deviceInfo/main.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 
 // main function of the application
-int main(int,char**)
+int main(int, char**)
 {
 	// catch exceptions
 	// (vulkan.hpp fuctions throw if they fail)
 	try {
 
 		// Vulkan version
-		auto vkEnumerateInstanceVersion=reinterpret_cast<PFN_vkEnumerateInstanceVersion>(
-				vkGetInstanceProcAddr(nullptr,"vkEnumerateInstanceVersion"));
+		auto vkEnumerateInstanceVersion = reinterpret_cast<PFN_vkEnumerateInstanceVersion>(
+			vkGetInstanceProcAddr(nullptr, "vkEnumerateInstanceVersion"));
 		if(vkEnumerateInstanceVersion) {
 			uint32_t version;
 			vkEnumerateInstanceVersion(&version);
-			cout<<"Vulkan instance version: "<<VK_VERSION_MAJOR(version)<<"."<<VK_VERSION_MINOR(version)
-			    <<"."<<VK_VERSION_PATCH(version)<<endl;
+			cout << "Vulkan instance version: " << VK_VERSION_MAJOR(version) << "."
+			     << VK_VERSION_MINOR(version) << "." << VK_VERSION_PATCH(version) << endl;
 		} else
-			cout<<"Vulkan version: 1.0"<<endl;
+			cout << "Vulkan version: 1.0" << endl;
 
 		// Vulkan instance
 		vk::UniqueInstance instance(
@@ -34,74 +34,73 @@ int main(int,char**)
 						VK_MAKE_VERSION(0,0,0),  // engine version
 						VK_API_VERSION_1_0,      // api version
 					},
-					0,nullptr,  // no layers
-					0,nullptr,  // no extensions
+					0, nullptr,  // no layers
+					0, nullptr,  // no extensions
 				}));
 
 		// print device info
-		vector<vk::PhysicalDevice> deviceList=instance->enumeratePhysicalDevices();
-		cout<<"Physical devices:"<<endl;
-		for(vk::PhysicalDevice pd:deviceList) {
+		vector<vk::PhysicalDevice> deviceList = instance->enumeratePhysicalDevices();
+		cout << "Physical devices:" << endl;
+		for(vk::PhysicalDevice pd : deviceList) {
 
 			// device properties
-			vk::PhysicalDeviceProperties p=pd.getProperties();
-			cout<<"   "<<p.deviceName<<endl;
+			vk::PhysicalDeviceProperties p = pd.getProperties();
+			cout << "   " << p.deviceName << endl;
 
 			// device Vulkan version
-			cout<<"      Vulkan version: "<<VK_VERSION_MAJOR(p.apiVersion)<<"."<<VK_VERSION_MINOR(p.apiVersion)
-			    <<"."<<VK_VERSION_PATCH(p.apiVersion)<<endl;
+			cout << "      Vulkan version: " << VK_VERSION_MAJOR(p.apiVersion) << "."
+			     << VK_VERSION_MINOR(p.apiVersion) << "." << VK_VERSION_PATCH(p.apiVersion) << endl;
 
 			// device limits
-			cout<<"      MaxTextureSize: "<<p.limits.maxImageDimension2D<<endl;
+			cout << "      MaxTextureSize: " << p.limits.maxImageDimension2D << endl;
 
 			// device features
-			vk::PhysicalDeviceFeatures f=pd.getFeatures();
-			cout<<"      Geometry shader: ";
+			vk::PhysicalDeviceFeatures f = pd.getFeatures();
+			cout << "      Geometry shader: ";
 			if(f.geometryShader)
-				cout<<"supported"<<endl;
+				cout << "supported" << endl;
 			else
-				cout<<"not supported"<<endl;
+				cout << "not supported" << endl;
 
 			// memory properties
-			vk::PhysicalDeviceMemoryProperties m=pd.getMemoryProperties();
-			cout<<"      Memory heaps:"<<endl;
-			for(uint32_t i=0,c=m.memoryHeapCount; i<c; i++)
-				cout<<"         "<<i<<": "<<m.memoryHeaps[i].size/1024/1024<<"MiB"<<endl;
+			vk::PhysicalDeviceMemoryProperties m = pd.getMemoryProperties();
+			cout << "      Memory heaps:" << endl;
+			for(uint32_t i=0, c=m.memoryHeapCount; i<c; i++)
+				cout << "         " << i << ": " << m.memoryHeaps[i].size/1024/1024 << "MiB" << endl;
 
 			// queue family properties
-			vector<vk::QueueFamilyProperties> queueFamilyList=pd.getQueueFamilyProperties();
-			cout<<"      Queue families:"<<endl;
-			for(uint32_t i=0,c=uint32_t(queueFamilyList.size()); i<c; i++) {
-				cout<<"         "<<i<<": ";
+			vector<vk::QueueFamilyProperties> queueFamilyList = pd.getQueueFamilyProperties();
+			cout << "      Queue families:" << endl;
+			for(uint32_t i=0, c=uint32_t(queueFamilyList.size()); i<c; i++) {
+				cout << "         " << i << ": ";
 				if(queueFamilyList[i].queueFlags & vk::QueueFlagBits::eGraphics)
-					cout<<"g";
+					cout << "g";
 				if(queueFamilyList[i].queueFlags & vk::QueueFlagBits::eCompute)
-					cout<<"c";
+					cout << "c";
 				if(queueFamilyList[i].queueFlags & vk::QueueFlagBits::eTransfer)
-					cout<<"t";
-				cout<<"  (count: "<<queueFamilyList[i].queueCount<<")";
-				cout<<endl;
+					cout << "t";
+				cout << "  (count: " << queueFamilyList[i].queueCount << ")" << endl;
 			}
 
 			// color attachment R8G8B8A8Unorm format support
 			vk::FormatProperties fp = pd.getFormatProperties(vk::Format::eR8G8B8A8Unorm);
-			cout<<"      R8G8B8A8Unorm format support for color attachment:"<<endl;
-			cout<<"         Images with linear tiling: "<<
-				string(fp.linearTilingFeatures & vk::FormatFeatureFlagBits::eColorAttachment ? "yes" : "no")<<endl;
-			cout<<"         Images with optimal tiling: "<<
-				string(fp.optimalTilingFeatures & vk::FormatFeatureFlagBits::eColorAttachment ? "yes" : "no")<<endl;
-			cout<<"         Buffers: "<<
-				string(fp.bufferFeatures & vk::FormatFeatureFlagBits::eColorAttachment ? "yes" : "no")<<endl;
+			cout << "      R8G8B8A8Unorm format support for color attachment:" << endl;
+			cout << "         Images with linear tiling: " <<
+				string(fp.linearTilingFeatures & vk::FormatFeatureFlagBits::eColorAttachment ? "yes" : "no") << endl;
+			cout << "         Images with optimal tiling: " <<
+				string(fp.optimalTilingFeatures & vk::FormatFeatureFlagBits::eColorAttachment ? "yes" : "no") << endl;
+			cout << "         Buffers: " <<
+				string(fp.bufferFeatures & vk::FormatFeatureFlagBits::eColorAttachment ? "yes" : "no") << endl;
 
 		}
 
 	// catch exceptions
-	} catch(vk::Error &e) {
-		cout<<"Failed because of Vulkan exception: "<<e.what()<<endl;
-	} catch(exception &e) {
-		cout<<"Failed because of exception: "<<e.what()<<endl;
+	} catch(vk::Error& e) {
+		cout << "Failed because of Vulkan exception: " << e.what() << endl;
+	} catch(exception& e) {
+		cout << "Failed because of exception: " << e.what() << endl;
 	} catch(...) {
-		cout<<"Failed because of unspecified exception."<<endl;
+		cout << "Failed because of unspecified exception." << endl;
 	}
 
 	return 0;
