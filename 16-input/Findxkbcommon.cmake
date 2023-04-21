e2168bf13249660c4a55a2e57b9fd05bf2e52d8b
PCJohn <peciva@fit.vut.cz>
2023-04-21 18:21:23 +0200

    16-input: Proper detection of xkbcommon for Xlib.

--- 16-input/Findxkbcommon.cmake
+++ 16-input/Findxkbcommon.cmake
@@ -1,12 +1,15 @@
 #
 # Module for finding xkbcommon
 #
-# Config-based find is attempted first.
-# If it fails standard detection is performed.
-# If successfull, xkbcommon::xkbcommon target will be created and following variables will be set:
+# If the find process is successfull, following variables will be set:
 #    xkbcommon_FOUND
 #    xkbcommon_INCLUDE_DIR
 #    xkbcommon_LIBRARY
+#
+# Following targets might be created:
+#    xkbcommon::xkbcommon - if xkbcommon_INCLUDE_DIR and xkbcommon_LIBRARY are set
+#    xkbcommon::headers - if xkbcommon_INCLUDE_DIR is set
+#    xkbcommon::lib - if xkbcommon_LIBRARY is set
 #
 
 
@@ -16,13 +19,6 @@ find_path(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR xkbcommon/xkbcommon.h
 	/usr/local/include
 	/opt/local/include
 )
-
-# message error level
-if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
-	set(errorLevel FATAL_ERROR)
-else()
-	set(errorLevel SEND_ERROR)
-endif()
 
 # find xkbcommon library
 find_library(${CMAKE_FIND_PACKAGE_NAME}_LIBRARY
@@ -36,22 +32,22 @@ find_library(${CMAKE_FIND_PACKAGE_NAME}_LIBRARY
 		/usr/local/lib
 )
 
-# set *_FOUND flag
+# set *_FOUND flag and make error message
 if(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR AND ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY)
 	set(${CMAKE_FIND_PACKAGE_NAME}_FOUND True)
 else()
-	# generate error message
-	message(${errorLevel} "Finding of package ${CMAKE_FIND_PACKAGE_NAME} failed. "
-				"Make sure it is installed and either "
-				"(1) ${CMAKE_FIND_PACKAGE_NAME}_DIR is set to the directory of "
-				"${CMAKE_FIND_PACKAGE_NAME}Config.cmake and ${CMAKE_FIND_PACKAGE_NAME}-config.cmake "
-				"or (2) ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR and "
-				"${CMAKE_FIND_PACKAGE_NAME}_LIBRARY are set properly.")
-	return()
+	set(${CMAKE_FIND_PACKAGE_NAME}_FOUND False)
+	if(${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
+		# generate error message
+		message(FATAL_ERROR "Finding of package ${CMAKE_FIND_PACKAGE_NAME} failed. "
+		        "Make sure it is installed and ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR "
+		        "and ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY are set properly.")
+		return()
+	endif()
 endif()
 
-# target
-if(${CMAKE_FIND_PACKAGE_NAME}_FOUND OR ${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
+# targets
+if(${CMAKE_FIND_PACKAGE_NAME}_FOUND)
 	if(NOT TARGET xkbcommon::xkbcommon)
 		add_library(xkbcommon::xkbcommon INTERFACE IMPORTED)
 		set_target_properties(xkbcommon::xkbcommon PROPERTIES
@@ -59,5 +55,21 @@ if(${CMAKE_FIND_PACKAGE_NAME}_FOUND OR ${CMAKE_FIND_PACKAGE_NAME}_FIND_REQUIRED)
 			INTERFACE_LINK_LIBRARIES "${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY}"
 		)
 		set(${CMAKE_FIND_PACKAGE_NAME}_DIR "${CMAKE_FIND_PACKAGE_NAME}_DIR-NOTFOUND" CACHE PATH "${CMAKE_FIND_PACKAGE_NAME} config directory." FORCE)
+	endif()
+endif()
+if(${CMAKE_FIND_PACKAGE_NAME}_LIBRARY)
+	if(NOT TARGET xkbcommon::lib)
+		add_library(xkbcommon::lib INTERFACE IMPORTED)
+		set_target_properties(xkbcommon::lib PROPERTIES
+			INTERFACE_LINK_LIBRARIES "${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY}"
+		)
+	endif()
+endif()
+if(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+	if(NOT TARGET xkbcommon::headers)
+		add_library(xkbcommon::headers INTERFACE IMPORTED)
+		set_target_properties(xkbcommon::lib PROPERTIES
+			INTERFACE_INCLUDE_DIRECTORIES "${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR}"
+		)
 	endif()
 endif()
